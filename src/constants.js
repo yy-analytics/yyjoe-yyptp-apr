56f0b30d3cb4f3d398c3aa4425b34e3424cd4eaf
Angus Hopkinson <angushopkinson@gmail.com>
2022-11-17 14:55:22 +0000

    Made changes so that it works for new Platypus MasterChef contract

--- src/constants.js
+++ src/constants.js
@@ -35,7 +35,7 @@ export const YYPTP_STAKING_ADDRESS = "0x9bc36cc686800be1905bf7e10578ee6fbdd6f27a
 
 export const PTP_YYPTP_PAIR_ADDRESS = "0x7a8ae10536d6920aa609d12775ffe6d73376668f";
 
-export const MASTER_PLATYPUSV3_ADDRESS = "0x68c5f4374228beedfa078e77b5ed93c28a2f713e";
+export const MASTER_PLATYPUSV4_ADDRESS = "0xff6934aac9c94e1c39358d4fdcf70aeca77d0ab0";
 
 export const PERCENTAGE_REWARDS_TO_YYPTP = 0.15;
 
@@ -43,12 +43,19 @@ export const YYPTP_STAKING_ABI = [
     { "inputs": [], "name": "internalBalance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }
 ];
 
-export const MASTER_PLATYPUSV3_ABI = [
-    { "inputs": [], "name": "ptpPerSec", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
-    { "inputs": [], "name": "totalAdjustedAllocPoint", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+export const MASTER_PLATYPUSV4_ABI = [
+    { "inputs": [], "name": "dilutingRepartition", "outputs": [{ "internalType": "uint16", "name": "", "type": "uint16" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "nonDilutingRepartition", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
     { "inputs": [], "name": "poolLength", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
-    { "inputs": [], "name": "dialutingRepartition", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
-    { "inputs": [], "name": "nonDialutingRepartition", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
-    { "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "name": "poolInfo", "outputs": [{ "internalType": "contract IAsset", "name": "lpToken", "type": "address" }, { "internalType": "uint256", "name": "baseAllocPoint", "type": "uint256" }, { "internalType": "uint256", "name": "lastRewardTimestamp", "type": "uint256" }, { "internalType": "uint256", "name": "accPtpPerShare", "type": "uint256" }, { "internalType": "contract IRewarder", "name": "rewarder", "type": "address" }, { "internalType": "uint256", "name": "sumOfFactors", "type": "uint256" }, { "internalType": "uint256", "name": "accPtpPerFactorShare", "type": "uint256" }, { "internalType": "uint256", "name": "adjustedAllocPoint", "type": "uint256" }], "stateMutability": "view", "type": "function" },
-    { "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }, { "internalType": "address", "name": "", "type": "address" }], "name": "userInfo", "outputs": [{ "internalType": "uint256", "name": "amount", "type": "uint256" }, { "internalType": "uint256", "name": "rewardDebt", "type": "uint256" }, { "internalType": "uint256", "name": "factor", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "name": "poolInfo", "outputs": [{ "internalType": "contract IAsset", "name": "lpToken", "type": "address" }, { "internalType": "contract IBoostedMultiRewarder", "name": "rewarder", "type": "address" }, { "internalType": "uint128", "name": "sumOfFactors", "type": "uint128" }, { "internalType": "uint128", "name": "accPtpPerShare", "type": "uint128" }, { "internalType": "uint128", "name": "accPtpPerFactorShare", "type": "uint128" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }, { "internalType": "address", "name": "", "type": "address" }], "name": "userInfo", "outputs": [{ "internalType": "uint128", "name": "amount", "type": "uint128" }, { "internalType": "uint128", "name": "factor", "type": "uint128" }, { "internalType": "uint128", "name": "rewardDebt", "type": "uint128" }, { "internalType": "uint128", "name": "claimablePtp", "type": "uint128" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "voter", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" },
+];
+
+export const PTP_VOTER_ABI = [
+    { "inputs": [], "name": "lpTokenLength", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "name": "lpTokens", "outputs": [{ "internalType": "contract IERC20", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "ptpPerSec", "outputs": [{ "internalType": "uint88", "name": "", "type": "uint88" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "totalWeight", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [{ "internalType": "contract IERC20", "name": "", "type": "address" }], "name": "weights", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
 ];
--- src/utils.js
+++ src/utils.js
@@ -18,6 +18,9 @@ import {
     PERCENTAGE_REWARDS_TO_YYPTP,
     YYPTP_STAKING_ABI,
     MASTER_PLATYPUSV3_ABI,
+    MASTER_PLATYPUSV4_ABI,
+    MASTER_PLATYPUSV4_ADDRESS,
+    PTP_VOTER_ABI,
 } from "./constants.js";
 
 // We use this to limit our calls if we are over the allowed rate for the free API.
@@ -203,70 +206,72 @@ export const getYyPTPAPR = async () => {
     const latestBlock = await getLatestBlockNumber();
     console.log(`Latest block has been retrieved, number = ${BigNumber.from(latestBlock).toNumber()}, hexValue = ${latestBlock}`);
     //
-    // Next we need the ptpPerSec emissions.
+    // Next step is to get the voter contract for MasterPlatypusV4 which contains a lot of important info.
     //
-    console.log("Getting the ptpPerSec for MasterPlatypusV3 contract...");
-    let ptpPerSec = await callContractFunction(MASTER_PLATYPUSV3_ABI, MASTER_PLATYPUSV3_ADDRESS, 'ptpPerSec', [], latestBlock);
+    console.log("Getting the voter contract for MasterPlatypusV4...");
+    const voterContract = await callContractFunction(MASTER_PLATYPUSV4_ABI, MASTER_PLATYPUSV4_ADDRESS, 'voter', [], latestBlock);
+    console.log(`voterContract = ${voterContract}`);
+    //
+    // Now we get some information from the voterContract.
+    //
+    console.log("Getting ptpPerSec and totalWeight...");
+    let ptpPerSec = await callContractFunction(PTP_VOTER_ABI, voterContract, 'ptpPerSec', [], latestBlock);
     ptpPerSec = convertWithDecimals(BigNumber.from(ptpPerSec).toString(), 18);
     const ptpPerYear = ptpPerSec * 60 * 60 * 24 * 365;
     console.log(`ptpPerSec = ${ptpPerSec} PTP`);
-    //
-    // Then need the totalAdjustedAllocPoint for the MasterPlatypusV3 contract.
-    // The reason we use the adjusted allocation points instead of totalBaseAllocPoint is that the adjusted values
-    // take into account the adjustment required to be made to interest rates in order to balance coverage ratios, and
-    // THESE are what the PTP rewards are given out on the basis of, not the baseAllocPoint.
-    //
-    console.log("Getting the totalAdjustedAllocPoint for MasterPlatypusV3 contract...");
-    let totalAdjustedAllocPoint = await callContractFunction(MASTER_PLATYPUSV3_ABI, MASTER_PLATYPUSV3_ADDRESS, 'totalAdjustedAllocPoint', [], latestBlock);
-    totalAdjustedAllocPoint = convertWithDecimals(BigNumber.from(totalAdjustedAllocPoint).toString(), 18);
-    console.log(`totalAdjustedAllocPoint = ${totalAdjustedAllocPoint}`);
+    let totalWeight = await callContractFunction(PTP_VOTER_ABI, voterContract, 'totalWeight', [], latestBlock);
+    totalWeight = convertWithDecimals(BigNumber.from(totalWeight).toString(), 18);
+    console.log(`totalWeight = ${totalWeight}`);
     //
     // Now we need the poolLength so that we can call poolInfo and userInfo functions for all pools.
     //
-    console.log("Getting the poolLength for MasterPlatypusV3 contract...");
-    let poolLength = await callContractFunction(MASTER_PLATYPUSV3_ABI, MASTER_PLATYPUSV3_ADDRESS, 'poolLength', [], latestBlock);
+    console.log("Getting the poolLength for MasterPlatypusV4 contract...");
+    let poolLength = await callContractFunction(MASTER_PLATYPUSV4_ABI, MASTER_PLATYPUSV4_ADDRESS, 'poolLength', [], latestBlock);
     poolLength = BigNumber.from(poolLength).toNumber();
     console.log(`poolLength = ${poolLength}`);
     //
-    // Now we get the dialuting and non-dialuting repartitions, which represent the share of PTP emissions going to "base" rewards
+    // Now we get the diluting and non-diluting repartitions, which represent the share of PTP emissions going to "base" rewards
     // and "boosted" rewards respectively, where base rewards are proportional to your deposits, and boosted rewards also depend
     // on your vePTP balance.
     //
-    console.log("Getting the dialutingRepartition and nonDialutingRepartition for MasterPlatypusV3 contract...");
-    let dialutingRepartition = await callContractFunction(MASTER_PLATYPUSV3_ABI, MASTER_PLATYPUSV3_ADDRESS, 'dialutingRepartition', [], latestBlock);
-    dialutingRepartition = BigNumber.from(dialutingRepartition).toNumber();
-    let nonDialutingRepartition = await callContractFunction(MASTER_PLATYPUSV3_ABI, MASTER_PLATYPUSV3_ADDRESS, 'nonDialutingRepartition', [], latestBlock);
-    nonDialutingRepartition = BigNumber.from(nonDialutingRepartition).toNumber();
-    console.log(`dialutingRepartition = ${dialutingRepartition}, nonDialutingRepartition = ${nonDialutingRepartition}`);
+    console.log("Getting the dilutingRepartition and nonDilutingRepartition for MasterPlatypusV4 contract...");
+    let dilutingRepartition = await callContractFunction(MASTER_PLATYPUSV4_ABI, MASTER_PLATYPUSV4_ADDRESS, 'dilutingRepartition', [], latestBlock);
+    dilutingRepartition = BigNumber.from(dilutingRepartition).toNumber();
+    let nonDilutingRepartition = await callContractFunction(MASTER_PLATYPUSV4_ABI, MASTER_PLATYPUSV4_ADDRESS, 'nonDilutingRepartition', [], latestBlock);
+    nonDilutingRepartition = BigNumber.from(nonDilutingRepartition).toNumber();
+    console.log(`dilutingRepartition = ${dilutingRepartition}, nonDilutingRepartition = ${nonDilutingRepartition}`);
     //
     // Now we create an array of poolIDs, and query the poolInfo and userInfo data from the Master Platypus contract.
     // We use this information, and everything else gathered so far to calculate the PTP earned per year.
     //
     const poolIDs = [...Array(poolLength).keys()];
-    console.log(`Getting the poolInfo and userInfo for MasterPlatypusV3 contract for our ${poolLength} pools...`);
+    console.log(`Getting the poolInfo and userInfo for MasterPlatypusV4 contract for our ${poolLength} pools...`);
     const poolUserInfo = await Promise.all(poolIDs.map(
         async (poolID) => {
-            const poolInfo = await callContractFunction(MASTER_PLATYPUSV3_ABI, MASTER_PLATYPUSV3_ADDRESS, 'poolInfo', [poolID], latestBlock);
-            let { lpToken, adjustedAllocPoint, sumOfFactors } = poolInfo;
-            adjustedAllocPoint = convertWithDecimals(BigNumber.from(adjustedAllocPoint).toString(), 18);
+            const poolInfo = await callContractFunction(MASTER_PLATYPUSV4_ABI, MASTER_PLATYPUSV4_ADDRESS, 'poolInfo', [poolID], latestBlock);
+            let { lpToken, sumOfFactors } = poolInfo;
             // Platypus LP token is 6 decimals and vePTP is 18 => sqrt of the multiplied combo is 12 decimals.
             sumOfFactors = convertWithDecimals(BigNumber.from(sumOfFactors).toString(), 12);
-            let totalLpSupply = await callContractFunction(LP_TOKEN_ABI, lpToken, 'balanceOf', [MASTER_PLATYPUSV3_ADDRESS], latestBlock);
+            // Now we get the weight from our voterContract.
+            let weight = await callContractFunction(PTP_VOTER_ABI, voterContract, 'weights', [lpToken], latestBlock);
+            weight = convertWithDecimals(BigNumber.from(weight).toString(), 18);
+            // Get the totalSupply for the base proportion.
+            let totalLpSupply = await callContractFunction(LP_TOKEN_ABI, lpToken, 'balanceOf', [MASTER_PLATYPUSV4_ADDRESS], latestBlock);
             // Platypus LP tokens have 6 decimals.
             totalLpSupply = convertWithDecimals(BigNumber.from(totalLpSupply).toString(), 6);
-            const userInfo = await callContractFunction(MASTER_PLATYPUSV3_ABI, MASTER_PLATYPUSV3_ADDRESS, 'userInfo', [poolID, YYPTP_ADDRESS], latestBlock);
+            const userInfo = await callContractFunction(MASTER_PLATYPUSV4_ABI, MASTER_PLATYPUSV4_ADDRESS, 'userInfo', [poolID, YYPTP_ADDRESS], latestBlock);
             let { amount, factor } = userInfo;
             amount = convertWithDecimals(BigNumber.from(amount).toString(), 6);
             factor = convertWithDecimals(BigNumber.from(factor).toString(), 12);
             return (
                 {
                     poolID,
-                    basePTPEarnedPerYear: adjustedAllocPoint === 0 ? 0 : ptpPerYear * adjustedAllocPoint * (dialutingRepartition / 1000) * amount / (totalAdjustedAllocPoint * totalLpSupply),
-                    boostedPTPEarnedPerYear: adjustedAllocPoint === 0 ? 0 : ptpPerYear * adjustedAllocPoint * (nonDialutingRepartition / 1000) * factor / (totalAdjustedAllocPoint * sumOfFactors),
+                    basePTPEarnedPerYear: weight === 0 ? 0 : ptpPerYear * weight * (dilutingRepartition / 1000) * amount / (totalWeight * totalLpSupply),
+                    boostedPTPEarnedPerYear: weight === 0 ? 0 : ptpPerYear * weight * (nonDilutingRepartition / 1000) * factor / (totalWeight * sumOfFactors),
                 }
             )
         }
-        ));
+    ));
     const totalPTPEarnedPerYear = poolUserInfo.reduce((a, b) => a + b.basePTPEarnedPerYear + b.boostedPTPEarnedPerYear, 0);
     console.log(`Total PTP earned per year = ${totalPTPEarnedPerYear} PTP`);
     //
